5d836a3f1ab5bc5f1c993284c87ed8cd0a72e66c
YanC Lin <yonalin1@gmail.com>
2018-04-02 15:37:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -145,6 +145,14 @@ class ApplicationController < Sinatra::Base
       redirect to '/'
     end
   end
-
+  
+  helpers do 
+     def is_logged_in?
+       !!current_user
+     end 
+     def current_user
+       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+     end 
+  end 
 
 end
